cf5b01e44b49aecc916c1fc0cbe03f99ec78ddd3
Roofing334 <Eric@sandsofsc.com>
2026-06-19 22:12:02 -0400

    Add HTTP server entrypoint

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const http = require("node:http");
+const { loadConfig } = require("./config");
+const { handleRequest, errorHandler } = require("./router");
+
+const config = loadConfig();
+
+const server = http.createServer(async (req, res) => {
+  try {
+    await handleRequest(req, res, config);
+  } catch (error) {
+    errorHandler(res, error);
+  }
+});
+
+server.listen(config.port, () => {
+  console.log(`JobNimbus gateway listening on http://localhost:${config.port}`);
+});
